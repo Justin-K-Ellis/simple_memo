599553d007d20825d4551ad37acd2b6f3de19414
Justin-K-Ellis <jlke45@gmail.com>
2025-11-16 23:16:28 +0900

    feat: fetch notes and render to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import db from "../db/index";
+import { notesTable } from "@/db/schema";
+
+export default async function Home() {
+  const notes = await db.select().from(notesTable);
+  console.log(notes);
+
   return (
     <>
-      <p>In the herp, party derp.</p>
+      {notes.map((note) => (
+        <div key={note.id}>
+          <h2>{note.title}</h2>
+          <p>{note.body}</p>
+          <p>Posted: {note.createdAt.toISOString()}</p>
+          {note.updatedAt !== null && (
+            <p>Update: {note.updatedAt.toISOString()}</p>
+          )}
+        </div>
+      ))}
     </>
   );
 }
