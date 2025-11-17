624a592b57a90cdc929e739fdc78eb6a191114a8
Justin-K-Ellis <jlke45@gmail.com>
2025-11-17 22:42:03 +0900

    fix: improve styling for links

--- src/app/components/NotePreview.tsx
+++ src/app/components/NotePreview.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import type { Note } from "@/db/schema";
 
 export default function NotePreview({ note }: { note: Note }) {
   return (
-    <div className="note-preview">
-      <h2>{note.title}</h2>
-      <p>{note.body}</p>
-      <hr />
-    </div>
+    <Link href={`/notes/${note.id}`}>
+      <div className="note-preview">
+        <h2>{note.title}</h2>
+        <p>{note.body}</p>
+        <hr />
+      </div>
+    </Link>
   );
 }
