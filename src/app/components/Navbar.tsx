427a9ef917e5e88f35613a2156956ecb363cd6e4
Justin-K-Ellis <jlke45@gmail.com>
2025-11-17 22:29:36 +0900

    fix: add link to title in navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <header>
       <nav className="navbar">
-        <h1>Simple Memo</h1>
+        <h1>
+          <Link href={"/"}>Simple Memo</Link>
+        </h1>
         <div className="login-btns">
           <button>Login</button>
           <button>Sign Up</button>
